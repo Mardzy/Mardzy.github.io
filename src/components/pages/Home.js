c23a105c11ca24a36a55fb64b79c311b1384fb9a
Mardzy <mardlingp@gmail.com>
2017-11-13 18:04:33 +0000

    changing styles add animations

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
- import React from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Row, Col } from 'reactstrap';
@@ -9,21 +9,64 @@ class Home extends React.Component {
     return (
       <div id="home">
         <Row className="no-gutters">
-          <Col md={6} sm={12}  className="home-logo">
-            <img src="src/images/icons/pm.png" className="rounded-circle logo" />
+          <Col md={6} xs={12} className="home-logo">
+            <div className="rounded-circle logo"><h1>PM</h1></div>
           </Col>
-          <Col md={6} sm={12}  className="home-about"></Col>
-          <Col md={6} sm={12}  className="home-intro">
-            <p>Welcome <br/> I`m Paul Mardling<br/>This is my portfolio</p>
+          <Col md={6} xs={12} className="home-about">
+            <Link to="about">
+              <h2>About Me</h2>
+            </Link>
           </Col>
-          <Col md={6} sm={12}  className="home-projects"></Col>
-          <Col md={6} sm={12}  className="home-contact"></Col>
+          <Col md={6} xs={12} className="home-intro">
+            <p>Welcome <br/> I'm Paul Mardling<br/>This is my portfolio</p>
+          </Col>
+          <Col md={6} xs={12} className="home-projects">
+            <Link to="projects">
 
-          <Col md={6} sm={12}  className="home-statement">
-            <p>Welcome <br/> I``m Paul Mardling<br/>This is my portfolio</p>
+              <h2>Projects</h2>
+            </Link>
           </Col>
-          <Col md={6} sm={12}  className="home-design"></Col>
-          <Col md={6} sm={12}  className="home-social">
+          <Col md={6} xs={12} className="home-contact">
+            <Link to="contact">
+              <h2>Contact</h2>
+            </Link>
+          </Col>
+
+          <Col md={6} xs={12} className="home-statement">
+            <p>Welcome <br/> I'm Paul Mardling<br/>This is my portfolio</p>
+          </Col>
+          <Col md={6} xs={12}  className="home-design">
+            <div className="pattern top rotate"></div>
+            <div className="pattern right"></div>
+            <div className="pattern bottom"></div>
+            <div className="pattern left"></div>
+            <div className="pattern top rotate"></div>
+            <div className="pattern right"></div>
+            <div className="pattern bottom"></div>
+            <div className="pattern left"></div>
+            <div className="pattern top rotate"></div>
+            <div className="pattern right"></div>
+            <div className="pattern bottom"></div>
+            <div className="pattern left"></div>
+            <div className="pattern top rotate"></div>
+            <div className="pattern right"></div>
+            <div className="pattern bottom"></div>
+            <div className="pattern left"></div>
+            <div className="pattern top rotate"></div>
+            <div className="pattern right"></div>
+            <div className="pattern bottom"></div>
+            <div className="pattern left"></div>
+            <div className="pattern left"></div>
+            <div className="pattern top rotate"></div>
+            <div className="pattern right"></div>
+            <div className="pattern bottom"></div>
+            <div className="pattern left"></div>
+            <div className="pattern top rotate"></div>
+            <div className="pattern right"></div>
+            <div className="pattern bottom"></div>
+            <div className="pattern left"></div>
+          </Col>
+          <Col md={6} xs={12}  className="home-social">
             <div className="social-icons rounded-circle">
               <Link to="https://github.com/Mardzy"><i className="fa fa-github-square" aria-hidden="true"></i></Link>
             </div>
