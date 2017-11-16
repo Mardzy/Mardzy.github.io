946970bccbf7e787c962cad1f06880ef5df08bb7
Mardzy <mardlingp@gmail.com>
2017-11-16 14:34:49 +0000

    create transition navigation

--- src/components/common/MainNav.js
+++ src/components/common/MainNav.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const MainNav = () => {
+const MainNav = ({toggle}) => {
 
   return (
     <section className="menu-wrapper">
       <nav id="main-menu">
-        <ul className="nav menu">
-          <li className="item-101 default"><Link to="/">Home</Link></li>
+        <ul id="transition" className="nav menu">
+          <li className="home-link default"><Link to="/">Home</Link></li>
           <li className="about-link current active"><Link to="/about">About</Link></li>
           <li className="projects-link"><Link to="/projects">Projects</Link></li>
           <li className="contact-link"><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
-      <a href="#" className="menu-toggle">
+      <a href="#" onClick={toggle} className="menu-toggle">
         <div id="nav-icon">
           <span></span>
           <span></span>
--- src/index.js
+++ src/index.js
@@ -3,34 +3,25 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
 import Routes from './components/utility/Routes';
 import MainNav from './components/common/MainNav';
-// const React = require('react');
-// const ReactDOM = require('react-dom');
-// const {BrowserRouter} = require('react-router-dom');
-// const Routes = require('./components/utility/Routes');
-// const MainNav = require('./components/common/MainNav');
-
 import 'bootstrap/dist/css/bootstrap.css';
-// require('bootstrap/dist/css/bootstrap.css');
-// require('./scss/style.scss');
 import './scss/style.scss';
 
 class App extends React.Component {
 
 
   componentDidMount(){
-    console.log('hello did mount');
-    const mainmenu = document.getElementById('main-menu');
+    const mainMenu = document.getElementById('transition');
+    console.log(mainMenu);
     const navIcon = document.getElementById('nav-icon');
     const toggleIcon = () =>{
       navIcon.addEventListener('click', ()=> {
         navIcon.classList.toggle('open');
-        mainmenu.classList.toggle('opened');
+        mainMenu.classList.toggle('opened');
       });
     };
     toggleIcon();
   }
   render(){
-    console.log('hello render');
     return (
       <BrowserRouter>
         <main>
